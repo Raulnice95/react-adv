5b802f2bd482e2d025ab1d545acb9a6e886b2c2b
Raul Nieva Cecilla <raulnice95@gmail.com>
2023-04-06 22:51:49 +0200

    LazyLoad Basico listo

---
+++ src/01-lazyload/pages/index.ts
@@ -0,0 +1,4 @@
+// Archivo para hacer varias importaciones al mismo tiempo
+export { LazyPage1 } from "./LazyPage1";
+export { LazyPage2 } from "./LazyPage2";
+export { LazyPage3 } from "./LazyPage3";
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,35 +1,46 @@
 import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom'
 import logo from '../logo.svg'
+import { routes } from './routes';
+import { Suspense } from 'react';
+
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-        <div className='main-layout'>
-            <nav>
-                <img src={ logo } alt="React Logo" />
-                <ul>
-                  <li>
-                    <NavLink to="/" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/users" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Users</NavLink>
-                  </li>
-                </ul>
-            </nav>
+    // El Suspense sirve para mostrar un componente cuando se esta cargando el componente al navegar a la ruta y aun no ha finalizado . 
+    // Mientras tanto, se muestra dicho componente, o en el caso de que haya una excepcion
+    <Suspense fallback={ <span>Loading...</span> }>
+      <BrowserRouter>
+          <div className='main-layout'>
+              <nav>
+                  <img src={ logo } alt="React Logo" />
+                  <ul>
+                    { routes.map(({to, name}) => (
+                      <li key={to}>
+                        <NavLink 
+                          to={ to } 
+                          className={ ({isActive}) => isActive ? 'nav-active' : '' }>
+                          {name}
+                        </NavLink></li>))}
+                  </ul>
+              </nav>
 
-            <Routes>
-              <Route path='about' element={ <h1>About</h1>} />
-              <Route path='users' element={ <h1>Users</h1>} />
-              <Route path='/' element={ <h1>Home Page</h1>} />
+              <Routes>
+                { 
+                  routes.map(({path, Component}) => (
+                    <Route 
+                      key={path} 
+                      path={ path } 
+                      element= { <Component />}
+                    />
+                  ))
+                }
 
-              {/* Para manejar la excepcion de navegar a una ruta no definida. Redirige al home y reemplaza la url incorrecta introducida por el home */}
-              <Route path='/*' element={ <Navigate to="/home" replace/>} />
-            </Routes>
+                {/* Para manejar la excepcion de navegar a una ruta no definida. Redirige al home y reemplaza la url incorrecta introducida por el home */}
+                <Route path='/*' element={ <Navigate to={ routes[0].to } replace/>} />
+              </Routes>
 
-        </div>
-    </BrowserRouter>
+          </div>
+      </BrowserRouter>
+    </Suspense>
   )
 }
